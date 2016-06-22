482daa55b14256744687fbac2fa0b8d12f092e3c
francisco2016 <otrayama@gmail.com>
2016-06-22 16:55:51 +0200

    -0049 add mt multiplesOfFive(), muestra multiplos de 5 en el intetvalo [15, 90] .

---
+++ Calculadora.java
@@ -0,0 +1,57 @@
+
+/**
+ * Autor Francisco.
+ */
+public class Calculadora
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Calculadora
+     */
+    public Calculadora()
+    {
+
+    }
+    
+    /**
+     *  imprima todos los múltiplos de 5 entre 10 y 95 (ambos extremos no incluidos) 
+     */
+    public void multiplesOfFive(){
+        int num =  15;
+        while(num < 95){
+            System.out.println(num);
+            num = num + 5;
+        }
+        
+    } 
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
